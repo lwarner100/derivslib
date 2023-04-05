01fca4e346fc76556350f97637acfe4e694b2310
Landon <lwarner75@gmail.com>
2023-04-04 22:30:35 -0400

    Setup fixes

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 requirements = [
-    'boto3==1.26.104',
-    'httplib2==0.21.0',
-    'ipywidgets==8.0.2',
-    'matplotlib>=3.5.1',
-    'numpy>=1.21.5',
+    'boto3',
+    'httplib2',
+    'ipywidgets',
+    'matplotlib',
+    'numpy',
     'pandas',
     'plotly',
     'requests',
@@ -17,12 +17,9 @@ requirements = [
 
 setup(
     name='derivslib',
-    version='0.0.6',
+    version='0.0.8',
     packages=find_packages(),
     install_requires=requirements,
-    entry_points={
-        'console_scripts': []
-    },
     description='Provides pricing tools and data for various derivative assets. I am not an attorney, accountant or financial advisor, nor am I holding myself out to be, and the information and tools contained in this package is not a substitute for financial advice from a professional who is aware of the facts and circumstances of your individual situation.',
     url='https://github.com/lwarner100/derivslib',
     license='MIT',
